e719f2f230fcb205ddabaf16ebf83467f67a4c03
Suzinino34 <suzinino24@gmail.com>
2024-12-04 10:26:56 -0800

    edit

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,46 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import ProductCard from '../Product/ProductCard'; 
-// import classes from '../Product/Product.module.css'
-// import Loader from '../Loader/Loader';
-// function Product() {
-//     const [products, setProducts] = useState([]);
-//     const [isLoading, setIsLoading] = useState(true);
-//     useEffect(() => {
-//         axios.get('https://fakestoreapi.com/products')
-//             .then((res) => {
-//                 setProducts(res.data);
-//                 isLoading(false)
-//             })
-//             .catch(error => {
-//                 console.error("Error fetching data: ", error);
-//                 isLoading(false)
-//             });
-//     }, []);
-
-//     return (
-
-//         <>
-//         {
-//             isLoading?(<Loader/>) : (<section className={classes.products_container}>
-//             {products.length > 0 ? (
-//                 products.map((singleProduct) => (
-//                     <ProductCard product={singleProduct} key={singleProduct.id} />
-//                 ))
-//             ) : (
-//                 <p>Loading products...</p>
-//             )}
-//         </section>)
-//         }
-        
-//         </>
-        
-//     );
-// }
-
-// export default Product;
-
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProductCard from '../Product/ProductCard'; 
@@ -78,7 +35,7 @@ function Product() {
                             <ProductCard renderAdd={true} product={singleProduct} key={singleProduct.id} />
                         ))
                     ) : (
-                        <p>No products found.</p> // Better to show a message when no products are found
+                        <p>No product found.</p> // Better to show a message when no products are found
                     )}
                 </section>
             )
