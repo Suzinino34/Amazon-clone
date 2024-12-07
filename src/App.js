8dc65be1a83613259cca75085c1134f74900311b
Suzinino34 <suzinino24@gmail.com>
2024-12-07 09:57:56 -0800

    fix the error

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Routing from './Router';
 
 
-
-
-
 function App() {
   return (
     <>
--- src/Components/carousel/Carousel.js
+++ src/Components/carousel/Carousel.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
-import {img} from './img/Data'
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import { img } from './img/Data';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import  classes from '../../Components/carousel/Carousel.module.css'
+import classes from '../../Components/carousel/Carousel.module.css';
+
 function CarouselEffect() {
   return (
     <div>
-<Carousel 
-    autoPlay={true}
-    infiniteLoop={true}
-    showIndicators={true}
-    showThumbs={false}
-
->
-{
-    img.map((imageItemLink)=>{
-        return <img src={imageItemLink}/>
-    })
-}
-
-
-</Carousel>
-<div className={classes.hero_img}></div>
-
+      <Carousel 
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={true}
+        showThumbs={false}
+      >
+        {
+          img.map((imageItemLink, index) => {
+            // Assuming imageItemLink is just a string (URL).
+            return <img key={index} src={imageItemLink} alt={`Carousel image ${index + 1}`} />;
+          })
+        }
+      </Carousel>
+      <div className={classes.hero_img}></div>
     </div>
-  )
+  );
 }
 
-export default CarouselEffect
+export default CarouselEffect;
